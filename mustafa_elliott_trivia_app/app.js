3ce95b5b5ee37d7bd597c2337a3375a57a4001a4
Syed Mustafa Imam <64550133+SyedMustafaImam@users.noreply.github.com>
2021-04-16 13:26:27 +0500

    Update app.js

--- mustafa_elliott_trivia_app/app.js
+++ mustafa_elliott_trivia_app/app.js
@@ -9,7 +9,7 @@ var session = require('express-session')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var adminRouter = require('./routes/admin');
-
+var issuance=require('./routes/issuance');
 
 var app = express();
 
@@ -51,7 +51,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/admin', adminRouter);
 app.use('/users', usersRouter);
-
+app.use('/issuance',issuance)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
