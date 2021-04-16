6de4514d935972667ec9d33d1d550a51627d3d5b
Elliott <cs1812110@szabist.pk>
2021-04-16 13:10:39 +0500

    backend login work

--- mustafa_elliott_trivia_app/controllers/issuanceDashBoard.controller.js
+++ mustafa_elliott_trivia_app/controllers/issuanceDashBoard.controller.js
@@ -21,4 +21,9 @@ exports.AddBooks=()=>{
         userid:req.body.userid,
         bookid:req.body.bookid
     })
+    add.save().then(result=>{
+        console.log(result)
+    }).catch(err=>{
+        console.log(err)
+    })
 }
