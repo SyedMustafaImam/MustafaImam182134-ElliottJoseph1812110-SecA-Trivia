9c6c42875ed3f00376d52303d7274ac887b211e0
Elliott Francis Joseph <73632496+Elliott2100@users.noreply.github.com>
2021-04-16 13:04:16 +0500

    Update issuance.js

--- mustafa_elliott_trivia_app/routes/issuance.js
+++ mustafa_elliott_trivia_app/routes/issuance.js
@@ -1,6 +1,6 @@
 const router = require('.')
 const issue_controller=require('../controllers/issuanceDashBoard.controller')
-router.get('/issuance/add',issue_controller.AddBooks)
+router.post('/issuance/add',issue_controller.AddBooks)
 router.post('/:id/update',issue_controller.ModifyBook)
 router.post('/:id/delete',issue_controller.DeleteBook)
 router.get('/list',issue_controller.ListBooks)
