df76f2423d9c92cc5987c3d4f73f4b3d03c08897
SyedMustafaImam <cs1812134@szabist.pk>
2021-05-06 13:52:37 +0500

    added

--- mustafa_elliott_trivia_app/app.js
+++ mustafa_elliott_trivia_app/app.js
@@ -7,7 +7,6 @@ var mongoose = require('mongoose');
 var session = require('express-session')
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var adminRouter = require('./routes/admin');
 
 
@@ -50,7 +49,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/admin', adminRouter);
-app.use('/users', usersRouter);
 
 
 // catch 404 and forward to error handler
