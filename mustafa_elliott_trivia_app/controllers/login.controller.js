f3a82f7f918692a81b0f8df04460dc4b47cfaa8c
Elliott <cs1812110@szabist.pk>
2021-05-11 16:28:43 +0500

    edited profile details

--- mustafa_elliott_trivia_app/controllers/login.controller.js
+++ mustafa_elliott_trivia_app/controllers/login.controller.js
@@ -8,16 +8,16 @@ exports.loginchk = (req, res) => {
     console.log(userid, "/", password);
     try {
         db.Member.findOne({ userid: userid, password: password }, (err, user) => {
-            console.log("User",user)
+            console.log("User", user)
             // req.session.user= user;
             // console.log("Session",req.session)
-            console.log(user.userid,"/",user.password)
+            console.log(user.userid, "/", user.password)
             if (user.userid === 'admin') {
-            
+
                 console.log('admin here')
-                req.session.user= user;
-                console.log("Session",req.session.user)
-                console.log(req.session.userid,"/",req.session.password)
+                req.session.user = user;
+                console.log("Session", req.session.user)
+                console.log(req.session.userid, "/", req.session.password)
                 res.redirect('index/admin')
             }
             else if (user === null) {
@@ -25,42 +25,48 @@ exports.loginchk = (req, res) => {
             }
             else if (user.userid === userid && user.password === password) {
                 console.log('got here to members')
-                req.session.user= user;
-                console.log("Session",req.session.user)
-                console.log(req.session.user.userid,"/",req.session.user.password)
-                res.redirect('index/member/'+req.session.user.userid)
+                req.session.user = user;
+                console.log("Session", req.session.user)
+                console.log(req.session.user.userid, "/", req.session.user.password)
+                res.redirect('index/member/' + req.session.user.userid)
             } else {
                 res.render('error');
             }
+            if (err) {
+                console.log('got here')
+                console.log(err)
+                res.redirect("/")
+            }
         })
-
     } catch (err) {
-        res.send(err)
+        console.log('got here')
+        console.log(err)
+        res.redirect("/")
     }
 }
-exports.edit_profile=(req,res)=>{
+exports.edit_profile = (req, res) => {
     console.log(req.params.userid);
-    db.Member.findOne({userid: req.params.userid}).then(users=>{
+    db.Member.findOne({ userid: req.params.userid }).then(users => {
         console.log(users)
         console.log('edit_profile form')
-        res.render('edit-profile', { title: 'Edit-Profile',session: users})
-    }).catch(err=>console.log(err))
-}     
-exports.edited_profile=async (req,res)=>{
+        res.render('edit-profile', { title: 'Edit-Profile', session: users })
+    }).catch(err => console.log(err))
+}
+exports.edited_profile = async (req, res) => {
     console.log(req.body)
     console.log(req.body.name)
     console.log(req.params.userid)
-    await db.Member.updateOne({userid :req.params.userid},{$set:req.body}).then(result=>{
-      console.log('part where update is done ')
-      console.log(result)
-      res.redirect('/index/member/'+req.body.userid)
-    }).catch(err=>console.log(err))
+    await db.Member.updateOne({ userid: req.params.userid }, { $set: req.body }).then(result => {
+        console.log('part where update is done ')
+        console.log(result)
+        res.redirect('/index/member/' + req.body.userid)
+    }).catch(err => console.log(err))
 }
 exports.logout = (req, res) => {
-    req.session.destroy((err)=>{
-        if(err){
+    req.session.destroy((err) => {
+        if (err) {
             return console.log(err)
-        }else{
+        } else {
             console.log('Destroying session');
         }
         res.redirect('/')
@@ -77,14 +83,25 @@ exports.logout = (req, res) => {
 //             console.log(req.session)
 //             res.render('member', { title: 'MEMBERS', session: req.session, user: user })
 //         })
-                
+
 //     }else{
 //         console.log(err)
 //     }
 // }
-exports.change_password_form=(req,res)=>{
- res.render('Passwordform',{title:'ChangePassword'})
+exports.change_password_form = (req, res) => {
+    console.log(req.session.user)
+    res.render('Passwordform', { title: 'ChangePassword', session: req.session.user })
 }
-exports.changed_password=(req,res)=>{
-   
+exports.changed_password = (req, res) => {
+    console.log(req.body)
+    console.log(req.params.userid)
+    if (req.body.newpassword===req.body.confirmpassword) {
+        db.Member.findOneAndUpdate({ userid: req.params.userid, password: req.body.oldpassword }, { $set: { password:req.body.newpassword } }).then(user => {
+            console.log(user)
+            res.redirect('/index/member/' + user.userid)
+        })
+    }else{
+        res.render('error')
+    }
+
 }
